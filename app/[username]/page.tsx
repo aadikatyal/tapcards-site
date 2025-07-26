2d1498bc80145c6fdafb0bd37244d24a6e97b848
Aadi Katyal <52411543+aadikatyal@users.noreply.github.com>
2025-07-25 20:04:40 -0400

    fix: mark profile page as client component to use useParams hook

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import { useParams } from 'next/navigation';
 
 export default function ProfilePage() {
   const params = useParams();
-  const username = params?.username;
+  const username = params.username;
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
