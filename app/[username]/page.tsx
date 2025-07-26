dac82afea5134fd404e59b7a940a2094b4d1105d
Aadi Katyal <52411543+aadikatyal@users.noreply.github.com>
2025-07-25 20:02:56 -0400

    feat: add dynamic username route and profile page template

---
+++ app/[username]/page.tsx
@@ -0,0 +1,13 @@
+import { useParams } from 'next/navigation';
+
+export default function ProfilePage() {
+  const params = useParams();
+  const username = params?.username;
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Profile Page for {username}</h1>
+      <p>This is where your contact card will appear.</p>
+    </div>
+  );
+}
